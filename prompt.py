d706251f92cf9cfcf4cc7587a86ad00cfb10a565
sxxgwoo <100654928+sxxgwoo@users.noreply.github.com>
2022-04-14 11:10:23 +0900

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,16 +1,21 @@
 import json
 
 def prompt_redesign(text_temp,name):
-    with open('./data_'+name+'.json','r') as json_file:
-        json_data = json.load(json_file)
-    tr = len(json_data)
-    i=1
-    while True:
-        if i<=tr:
-            text_history = 'You:'+json_data[str(i)][0]+'\nFriend:'+json_data[str(i)][1]
-            text = text_history+'\nYou:'+text_temp+'\nFriend:'
-            i+=1
-        else:
-            break
-    
-    return text
+    if os.path.exists('./data_'+name+'.json'):
+        with open('./data_'+name+'.json','r') as json_file:
+            json_data = json.load(json_file)
+        tr = len(json_data)
+        i=1
+        while True:
+            if i<=tr:
+                text_history = 'You:'+json_data[str(i)][0]+'\nFriend:'+json_data[str(i)][1]
+                text = text_history+'\nYou:'+text_temp+'\nFriend:'
+                i+=1
+            else:
+                break
+
+        return text
+    else:
+         text ='\nYou:'+text_temp+'\nFriend:'
+
+         return text
