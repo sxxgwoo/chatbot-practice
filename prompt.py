01d38f5b0cf667aac9b9087cec65a5a1c6be5be4
sxxgwoo <100654928+sxxgwoo@users.noreply.github.com>
2022-04-15 09:24:49 +0900

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,21 +1,34 @@
 import json
+import os
 
-def prompt_redesign(text_temp,name):
+def prompt_redesign(text_temp,name,term):
     if os.path.exists('./data_'+name+'.json'):
         with open('./data_'+name+'.json','r') as json_file:
             json_data = json.load(json_file)
         tr = len(json_data)
         i=1
+        text_history = str()
         while True:
-            if i<=tr:
-                text_history = 'You:'+json_data[str(i)][0]+'\nFriend:'+json_data[str(i)][1]
-                text = text_history+'\nYou:'+text_temp+'\nFriend:'
-                i+=1
+            if int(term)==-1:
+                if i<=tr:
+                    text_history += 'You:'+json_data[str(i)][0]+'\nFriend:'+json_data[str(i)][1]+'\n'
+                    text = text_history+'You:'+text_temp+'\nFriend:'
+                    i+=1
+                else:
+                    break
+            elif int(term)>0:
+                if i<=tr-int(term):
+                    text_history += 'You:'+json_data[str(i)][0]+'\nFriend:'+json_data[str(i)][1]+'\n'
+                    text = text_history+'You:'+text_temp+'\nFriend:'
+                    i+=1
+                else:
+                    break            
             else:
+                text ='\nYou:'+text_temp+'\nFriend:'
                 break
-
+        
         return text
     else:
-         text ='\nYou:'+text_temp+'\nFriend:'
+        text ='\nYou:'+text_temp+'\nFriend:'
 
-         return text
+        return text
